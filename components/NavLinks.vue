ced8df3a7e869312da0c101356f9f4671085a83a
dvogelg4al <dvogel@g4al.com>
2023-11-15 15:03:42 +0100

    init navlinks component

---
+++ components/NavLinks.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtLink class="underline text-fuchsia-800" to="/">Home</NuxtLink>
+  <NuxtLink class="underline text-fuchsia-800" to="/cats">Cats</NuxtLink>
+  <NuxtLink class="underline text-fuchsia-800" to="/about">About</NuxtLink>
+</template>
