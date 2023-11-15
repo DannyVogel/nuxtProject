a8c426788ed305f8b666134b688abbf5cf5936e4
dvogelg4al <dvogel@g4al.com>
2023-11-15 15:03:56 +0100

    init

---
+++ components/Header/Logo.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import gif from "../../assets/giphy (8).gif";
+</script>
+<template>
+  <img :src="gif" class="w-20" />
+</template>
---
+++ components/Header/Text.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps({
+  text: String,
+});
+</script>
+<template>
+  <p>{{ text }}</p>
+</template>
