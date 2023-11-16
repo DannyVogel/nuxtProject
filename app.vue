5aa018a9bc0c04b1b1d29b825ba45e9b66f2de3e
dvogelg4al <danny.hj.vogel@gmail.com>
2023-11-16 23:22:04 +0100

    cleanup

--- app.vue
+++ app.vue
@@ -1,21 +1,3 @@
-<script setup lang="ts">
-const nuxtApp = useNuxtApp();
-const init = async () => {
-  try {
-    await nuxtApp.$web3auth.init();
-    nuxtApp.$provider.value = nuxtApp.$web3auth.provider;
-    console.log("init", nuxtApp.$provider.value);
-  } catch (error) {
-    console.error(error);
-  }
-};
-onMounted(async () => {
-  console.log("app provider", nuxtApp.$provider);
-  console.log("app web3auth", nuxtApp.$web3auth);
-  await init();
-});
-</script>
-
 <template>
   <NuxtLayout>
     <NuxtPage />
