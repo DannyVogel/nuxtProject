b68078d27b0fc6d19643584865bd682b20019ad5
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2023-11-16 23:49:40 +0100

    remove logs

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    console.log('Coming from', from.path)
-    console.log('Heading to', to.path)
+    // console.log('Coming from', from.path)
+    // console.log('Heading to', to.path)
   })
